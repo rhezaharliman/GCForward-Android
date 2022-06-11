125c6ebabd2f33e579c33ae5a778cdcedf64c6b5
Rheza Harliman <rheza.harliman@gmail.com>
2022-06-11 16:28:28 +0900

    Imp #7 : implement search screen This commit implements the search screen. There are still a lot of tests to do like * when the user list shows up. * Error when the edit text is empty. * etc.
    Those tests above will be implemented to gether with issue #8.

--- app/src/main/java/com/rheza/gcforward/MainActivity.kt
+++ app/src/main/java/com/rheza/gcforward/MainActivity.kt
@@ -2,10 +2,24 @@ package com.rheza.gcforward
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mSearchButton: Button
+    lateinit var mProgressBar:ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mSearchButton = findViewById(R.id.btn_search)
+        mProgressBar = findViewById(R.id.progress_circular)
+        mSearchButton.setOnClickListener(searchButtonListener)
+    }
+
+    val searchButtonListener = View.OnClickListener {
+        // TODO: if search operation is on the run, deactivate the button
+        mProgressBar.visibility = View.VISIBLE
     }
 }
